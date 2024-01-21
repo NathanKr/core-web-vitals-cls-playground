63c4c8ed720606722bee64be991020dafc32c82e
nathankr <natankrasney@gmail.com>
2024-01-21 20:46:42 +0200

    ...

--- src/pages/keep-aspect-ratio-solution-with-bound-parent.tsx
+++ src/pages/keep-aspect-ratio-solution-with-bound-parent.tsx
@@ -10,7 +10,7 @@ import {
 import { Alert } from "@mui/material";
 
 const KeepAspectRatioSolutionWithBoundParent = () => {
-  const aspectRatio = LION_IMG_WIDTH_PX / LION_IMG_HEIGHT_PX;
+  const aspectRatio = 1 / LION_IMG_WIDTH_PX / LION_IMG_HEIGHT_PX;
   const paddingTopPercentage = `${(100 * 1) / aspectRatio}%`;
   console.log(paddingTopPercentage);
   
